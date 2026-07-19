13f5bbd9f30f6d4ab304038f2c3303eb07e420f6
Sandeep Kumar <shreemansandeep@example.com>
2026-07-19 16:21:46 +0530

    cleanup: remove VPC and EC2 configuration and variables

--- variables.tf
+++ variables.tf
@@ -22,29 +22,3 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.11.0/24"]
-}
-
-variable "database_subnet_cidrs" {
-  description = "CIDR blocks for database subnets"
-  type        = list(string)
-  default     = ["10.0.20.0/24", "10.0.21.0/24"]
-}
-
-
-
