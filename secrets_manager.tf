3c17c28103e8af7cefa17af85b03152c49729979
Sandeep Kumar <shreemansandeep@example.com>
2026-07-19 23:38:17 +0530

    Comment out secrets manager configuration

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,20 +1,20 @@
-resource "random_password" "db_password" {
-  length  = 20
-  special = true
-}
+# resource "random_password" "db_password" {
+#   length  = 20
+#   special = true
+# }
 
-resource "aws_secretsmanager_secret" "mysql" {
-  name = "prod/mysql"
-}
+# resource "aws_secretsmanager_secret" "mysql" {
+#   name = "prod/mysql"
+# }
 
-resource "aws_secretsmanager_secret_version" "mysql" {
+# resource "aws_secretsmanager_secret_version" "mysql" {
 
-  secret_id = aws_secretsmanager_secret.mysql.id
+#   secret_id = aws_secretsmanager_secret.mysql.id
 
-  secret_string = jsonencode({
+#   secret_string = jsonencode({
 
-    username = "admin"
+#     username = "admin"
 
-    password = random_password.db_password.result
-  })
-}
+#     password = random_password.db_password.result
+#   })
+# }
