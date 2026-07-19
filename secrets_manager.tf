54ac408f2fd8920a87d0a087da45c6b1fe6422ec
Sandeep Kumar <shreemansandeep@example.com>
2026-07-19 23:23:43 +0530

    Add Secrets Manager configuration for MySQL credentials

---
+++ secrets_manager.tf
@@ -0,0 +1,20 @@
+resource "random_password" "db_password" {
+  length  = 20
+  special = true
+}
+
+resource "aws_secretsmanager_secret" "mysql" {
+  name = "prod/mysql"
+}
+
+resource "aws_secretsmanager_secret_version" "mysql" {
+
+  secret_id = aws_secretsmanager_secret.mysql.id
+
+  secret_string = jsonencode({
+
+    username = "admin"
+
+    password = random_password.db_password.result
+  })
+}
